59d700d16791a6ff10030761a376a3c40a319152
vermad843 <vishnuverma7524@gmail.com>
2021-04-09 21:48:41 +0530

    input and button

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ function App() {
           <p className = "app__body__left__para">
              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
+          <div className = "app__body__left__container">
+             <input
+                className = "app__body__left__container__input"
+               />
+             <button 
+                className = "app__body__left__container__button"
+               >
+                Signup
+             </button>
+          </div>
       </div>
       <div className = "app__body__right">
           <h1 className = "app__body__right__head">Where the world builds software</h1> 

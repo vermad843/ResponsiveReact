0108d9648d9e52fc69f50d856d9563e855dc8352
vermad843 <vishnuverma7524@gmail.com>
2021-04-14 08:32:57 +0530

    different way

---
+++ README.md
@@ -0,0 +1,10 @@
+[x]  use ref hooks(for pause and play video)
+
+[x] React-Router-dom
+
+[x] useHistory
+
+[x] Material-ui(for icons)
+
+[x] Date range picker
+
--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React from 'react'
 import './App.css';
-import Header from './Header';
+import Header from './component/Header';
+import Body from './component/Body';
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <div className = "app__body">
-      <div className = "app__body__left">
-          <h1 className = "app__body__left__head">Where the world builds software</h1>
-          <p className = "app__body__left__para">
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div className = "app__body__left__container">
-             <input
-                className = "app__body__left__container__input"
-               />
-             <button 
-                className = "app__body__left__container__button"
-               >
-                Signup
-             </button>
-          </div>
-      </div>
-      <div className = "app__body__right">
-          <h1 className = "app__body__right__head">Where the world builds software</h1> 
-          <p className = "app__body__right__para">
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>     
-      </div>
-      </div>
-      <hr/>
-      <div className = "footer">
-        <h1>All Rights Reserved @2021</h1>
-      </div>
-    </div>
+     <div>
+        <Header/>
+        <Body/>
+     </div>
   );
 }
 
